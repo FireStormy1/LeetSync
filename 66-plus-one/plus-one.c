f585a91d595052d2a1900a82621408d522e319f1
Saswat Dixit <85784995+FireStormy1@users.noreply.github.com>
2026-07-01 09:07:00 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (1.69%) - LeetSync

---
+++ 66-plus-one/plus-one.c
@@ -0,0 +1,19 @@
+int* plusOne(int* digits, int digitsSize, int* returnSize) {
+    for (int i = digitsSize - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;
+            *returnSize = digitsSize;
+            int* result = (int*)malloc(digitsSize * sizeof(int));
+            for (int j = 0; j < digitsSize; j++) {
+                result[j] = digits[j];
+            }
+            return result;
+        }
+        digits[i] = 0;
+    }
+    *returnSize = digitsSize + 1;
+    int* result = (int*)calloc(*returnSize, sizeof(int)); 
+    result[0] = 1;
+    
+    return result;
+}
